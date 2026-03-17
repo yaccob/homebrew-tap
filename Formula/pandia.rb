9c77e573080ab5df9cd2dc6fe733a84b485e29cc
Jakob Stemberger <jakob.stemberger@gmail.com>
2026-03-18 00:48:20 +0100

    Add pandia formula v1.0.0

---
+++ Formula/pandia.rb
@@ -0,0 +1,33 @@
+class Pandia < Formula
+  desc "Markdown to PDF/HTML with PlantUML, Graphviz, Mermaid, Ditaa and LaTeX math"
+  homepage "https://github.com/yaccob/pandia"
+  url "https://github.com/yaccob/pandia/archive/refs/tags/v1.0.0.tar.gz"
+  version "1.0.0"
+  license "MIT"
+
+  depends_on "pandoc"
+  depends_on "plantuml"
+  depends_on "graphviz"
+  depends_on "mermaid-cli"
+  depends_on "librsvg"
+  depends_on "basictex" => :optional
+
+  def install
+    share.install "diagram-filter.lua" => "pandia/diagram-filter.lua"
+    bin.install "bin/pandia"
+  end
+
+  def caveats
+    <<~EOS
+      pandia requires a LaTeX distribution for PDF output.
+      If you don't have one, install BasicTeX:
+        brew install --cask basictex
+    EOS
+  end
+
+  test do
+    (testpath/"test.md").write("# Hello\n\n$$E=mc^2$$\n")
+    system bin/"pandia", "--html", "test.md"
+    assert_predicate testpath/"test.html", :exist?
+  end
+end
